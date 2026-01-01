28b6b9e037adeb1f3700cb897c2192167e0ae1fe
Gulbaz3032 <gulbazkachoo3032@gmail.com>
2026-01-01 22:23:48 +0500

    some code on login controlller

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -103,6 +103,27 @@ export const isVerify = async (req, res) => {
 
 export const loginUser = async (req, res) => {
   try {
+    const { email, password } = req.body;
+    if(!email || !password) {
+      return res.status(400).json({
+        message: "Fields are required",
+        success: false
+      })
+    }
+
+    const userPassword = await User(password, userPassword);
+    if(userPassword != password) {
+      return res.status(400).json({
+        message: "Invalid email or password"
+      })
+    }
+
+    return res.status(200).json({
+      message: "User login successfully",
+      success: true,
+      token: token
+    })
+
   } catch (error) {
     return res.status(500).json({
       message: "Failed to Login user, Server error",
